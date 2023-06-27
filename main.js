df38c213dbc51c63a01fe586a811ff7c9e949ee0
lashasordia123 <102297392+lashasordia123@users.noreply.github.com>
2023-06-27 22:09:38 +0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,4 @@
+let firstImage = "https://img.pikbest.com/png-images/creative-colorful-camera-logo-design-symbol-vector_1633940.png!f305cw";
+document.querySelector("head").innerHTML += `
+<link rel="shortcut icon" href="${firstImage}" type="image/x-icon">
+`
